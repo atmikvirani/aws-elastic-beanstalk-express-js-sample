f7fc0c745e52ccaedb169a9a6853ff20dcac0f36
Atmik Virani <atmikvirani@gmail.com>
2024-06-13 00:25:24 -0400

    Final Result

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Jay Swaminarayan from AWS'));
+app.get('/', (req, res) => res.send('I have created my first Continuous Delivery Pipeline and here is the Result'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
